81c50ecc6e0adf2d2912dc073aedcd821a05a77f
Leticia Lima <leticiaalima77@gmail.com>
2025-04-30 09:01:46 -0300

    04

---
+++ 04/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int numero = 2;
+
+        while (numero <= N)
+        {
+            Console.WriteLine(numero);
+            numero += 2; 
+        }
+    }
+}
+
